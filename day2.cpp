9eccde98339fb159325f6c6bb7c3d0bd3e990069
NAITIK RAJPUT <naitikrajput.26.04@gmail.com>
2026-09-09 15:57:30 +0530

    2

---
+++ day2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+using namespace std ;
+void function ( ){
+    unique_ptr<int>ptr(new int (50));
+    cout<<*ptr<<endl;
+    cout<<&ptr;
+    cout<<ptr;
+    
+}
+
+int main()
+{
+    function();
+    return 0;
